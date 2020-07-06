b223975ea06443bddc0069ffd02bb048cad86d69
codingwithkat <codingwithkat@users.noreply.github.com>
2020-07-06 11:52:06 -0400

    Jumbotron sizing css

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import styled from "styled-components";
 import jumbocode from '../images/jumbocode.JPG';
+import "../css/Jumbotron.css";
 
 
 const Styles = styled.div`
@@ -13,7 +14,6 @@ const Styles = styled.div`
         position: relative;
         padding-top: 4px;
     }
-
 `;
 
 export const Jumbotron = () => (
