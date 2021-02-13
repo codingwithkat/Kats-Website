bb4cad6a4fe6feef2d85b5941ba91ac77400491e
Katerina Scoullos <katpapaloukas@gmail.com>
2021-02-13 00:05:48 -0500

    fix error

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,7 +19,7 @@ export const Projects = () => (
                     <img className="card-img-top" src={comic} alt="project"/>
                     <img className="card-img-top" src={comic2} alt="project"/>
                     <div className="card-body">
-                        <h5 className="card-title"><a href="http://comicsans-app.herokuapp.com/" target="_blank" >Comic Sans</a></h5>
+                        <h5 className="card-title"><a href="http://comicsans-app.herokuapp.com/">Comic Sans</a></h5>
                         <p className="card-text">Comic page creator!<p className="stack-info">Tech Stack: React, Redux, Heroku, HTML5, CSS3, Bootstrap, Express, Node, Sequelize, Fabric, HTML Canvas</p></p>
                         
                     </div>
@@ -29,7 +29,7 @@ export const Projects = () => (
                 <div className="card" style={{ width: '30rem' }}>
                     <img className="card-img-top" src={artemis} alt="project"/>
                     <div className="card-body">
-                        <h5 className="card-title"><a href="https://artemis-workouts.herokuapp.com/home" target="_blank">Artemis Workouts</a></h5>
+                        <h5 className="card-title"><a href="https://artemis-workouts.herokuapp.com/home">Artemis Workouts</a></h5>
                         <p className="card-text">An exercise library with the ability to create individualized daily workout plan! 
                         <p className="stack-info">Tech Stack: React, Redux, Heroku, HTML5, CSS3, Bootstrap, Express, Node, Google OAuth, SQL, Sequelize</p></p>
                         
@@ -42,7 +42,7 @@ export const Projects = () => (
                 <div className="card" style={{ width: '30rem' }}>
                     <img className="card-img-top" src={skincare} alt="project"/>
                     <div className="card-body">
-                        <h5 className="card-title"><a href="https://gh-skincare1.herokuapp.com/" target="_blank">E-commerce SkinCare</a></h5>
+                        <h5 className="card-title"><a href="https://gh-skincare1.herokuapp.com/" >E-commerce SkinCare</a></h5>
                         <p className="card-text">Skincare e-commerce website! <p className="stack-info">Tech Stack: React, Redux, Heroku, HTML5, CSS3, Bootstrap, Express, Node, Google OAuth, SQL, Sequelize</p></p>
                         
                     </div>
@@ -52,7 +52,7 @@ export const Projects = () => (
                 <div className="card" style={{ width: '30rem' }}>
                     <img className="card-img-top" src={weather} alt="project"/>
                     <div className="card-body">
-                        <h5 className="card-title"><a href="https://react-city-weather-app.netlify.app/" target="_blank">React City Weather App</a></h5>
+                        <h5 className="card-title"><a href="https://react-city-weather-app.netlify.app/">React City Weather App</a></h5>
                         <p className="card-text">A weather app that provides you with weather information by city! <p className="stack-info">Tech stack: React, Javascript, HTML, CSS, Bootstrap</p></p>
                     </div>
                 </div>
