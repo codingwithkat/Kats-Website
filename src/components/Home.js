65161da4437136a80401bec9135236ec9b490a3e
Katerina Scoullos <katpapaloukas@gmail.com>
2021-03-17 13:50:18 -0400

    edit spelling

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,7 +19,7 @@ export const Home = () => (
                     <p className="intro-kat">Hello! My name is Katerina and I am a full stack web developer looking to make an impact in tech as a developer/product manager!</p>
                    <p>
                     I have spent the last 7 years in
-                    the Education Industry specifically focused on Business Operations in the Franchise world utilizing my communication and project management skills. In April of 2020, I decided to take a course on HTML and the rest was history. I quickly made the decision to gain the necassary technical skills to transition into the tech industry as a developer/product manager!
+                    the Education Industry specifically focused on Business Operations in the Franchise world utilizing my communication and project management skills. In April of 2020, I decided to take a course on HTML and the rest was history. I quickly made the decision to gain the necessary technical skills to transition into the tech industry as a developer/product manager!
                     </p>
                     <p>
                     Since then, I have been teaching myself how to code and develop web applications. As a result of this new found passion, I attended the <strong className="gh">Grace Hopper Program through the Fullstack Academy</strong> and graduated with a certificate in Full Stack Web Development in <strong className="gh">February 2021! </strong>
